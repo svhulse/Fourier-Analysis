38c8fa69906a42f106710dfef93eac094da17210
svhulse <sam@X395.localdomain>
2019-08-26 16:14:23 +0200

    updates

--- plots.py
+++ plots.py
@@ -15,6 +15,8 @@ from matplotlib import pyplot as plt
 import matplotlib.patches as patches
 from numpy.fft import fft2, fftshift
 
+from pspec import *
+
 __author__ = 'Samuel Hulse'
 __email__ = 'hsamuel1@umbc.edu'
 
@@ -37,80 +39,6 @@ def random_sample(data, sample_dim):
     sample = data[sample_x: sample_x + sample_dim, sample_y: sample_y + sample_dim]
 
     return sample
-def rotavg(data):
-    center = np.divide(data.shape, 2)
-    x_sample = np.linspace(0, data.shape[0], data.shape[0])
-    y_sample = np.linspace(0, data.shape[1], data.shape[1])
-    x, y = np.meshgrid(y_sample, x_sample)
-    
-    x = np.absolute(x - center[1])
-    y = np.absolute(y - center[0])
-    dist_matrix = np.sqrt(x**2 + y**2)
-
-    max_dist = np.sqrt(np.sum(np.square(center)))
-    n_bins = int(np.ceil(max_dist))
-    bins = np.linspace(0, max_dist, n_bins)
-    
-    radialprofile = np.zeros(n_bins - 1)
-    
-    for i in range(len(bins[0:-1])):
-        filter = np.zeros(data.shape)
-        filter[np.logical_and(
-            dist_matrix >= bins[i], 
-            dist_matrix < bins[i+1])] = 1
-        
-        radialprofile[i] = np.sum(filter * data) / np.sum(filter)
-    
-    return radialprofile
-
-def bin_pspec(data, n_bins, bin_range):
-    bins = np.logspace(np.log(bin_range[0]), 
-        np.log(bin_range[1]),
-        n_bins,
-        base = np.e)
-    x = np.linspace(1, len(data), len(data))
-
-    bins_x = []
-    bins_y = []
-
-    for i in range(len(bins[0:-1])):
-        bin_x = np.mean(x[np.logical_and(
-            x >= bins[i],
-            x < bins[i+1])])
-        bins_x.append(bin_x)
-
-        bin_y = np.mean(data[np.logical_and(
-            x >= bins[i],
-            x < bins[i+1])])
-        bins_y.append(bin_y)
-    
-    return (bins_x, bins_y)
-
-def get_slope(x, y):
-    x, y = np.log((x, y))
-    slope = np.polyfit(x, y, 1)[0]
-
-    return slope
-
-def kaiser2D(img, alpha):
-    #Calculate the 2D Kaiser-Bessel window as the outer product of the 1D window with itself
-    kaiser = np.kaiser(img.shape[0], alpha*np.pi)
-    A = np.outer(kaiser, kaiser)
-    
-    #Normalize by the sum of squared weights
-    w = np.sum(A*A)
-    A = A / w
-
-    #Apply the window by performing elementwise multiplication
-    imout = np.multiply(img, A)
-    return imout
-
-def imfft(image):
-    imfft = fftshift(fft2(image))
-    impfft = np.absolute(imfft) ** 2
-    pspec = rotavg(impfft)
-
-    return pspec
 
 #Habitat Violinplot
 f, ax = plt.subplots()
